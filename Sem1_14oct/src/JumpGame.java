3035fd7c733666e55d881bc56b65d859d886c783
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2023-10-14 14:17:03 +0300

    jumpGame solution repaired

--- Sem1_14oct/src/JumpGame.java
+++ Sem1_14oct/src/JumpGame.java
@@ -1,7 +1,21 @@
+import java.util.Arrays;
+
 public class JumpGame {
-    public static boolean canWin(int[] array) {
-        for (int i = 0; i < array.length - 1; ++i)
-            if (i + array[i] >= array.length - 1) return true;
-        return false;
+
+    private static int getSum(int[] arr, int from, int to) {
+        return arr[from] - arr[to + 1];
     }
+
+    public static boolean canWin(int[] array) {
+        int[] winning = new int[array.length];
+        int[] sums = new int[array.length + 1];
+        winning[array.length - 1] = 1;
+        sums[array.length - 1] = 1;
+        for (int i = array.length - 2; i >= 0; --i) {
+            winning[i] = (getSum(sums, i + 1, Math.min(array.length - 1, i + array[i])) > 0 ? 1 : 0);
+            sums[i] = sums[i + 1] + winning[i];
+        }
+        return winning[0] == 1;
+    }
+
 }
