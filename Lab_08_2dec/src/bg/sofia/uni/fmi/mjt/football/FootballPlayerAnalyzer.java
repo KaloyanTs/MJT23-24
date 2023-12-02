437a88b087e483559fe74dfb3e98ee4b3a6f40b2
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2023-12-02 15:08:00 +0200

    Database, failing test and implementation of methods

--- Lab_08_2dec/src/bg/sofia/uni/fmi/mjt/football/FootballPlayerAnalyzer.java
+++ Lab_08_2dec/src/bg/sofia/uni/fmi/mjt/football/FootballPlayerAnalyzer.java
@@ -1,4 +1,137 @@
 package bg.sofia.uni.fmi.mjt.football;
 
+import java.io.Reader;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class FootballPlayerAnalyzer {
+
+    final private static int MAX_POTENTIAL_DIFFERENCE_FOR_SIMILARITY = 3;
+
+    Stream<Player> players;
+
+    /**
+     * Loads the dataset from the given {@code reader}. The reader argument will not be null and a correct dataset of
+     * the specified type can be read from it.
+     *
+     * @param reader Reader from which the dataset can be read.
+     */
+    public FootballPlayerAnalyzer(Reader reader) {
+        throw new UnsupportedOperationException("Method not yet implemented");
+    }
+
+    /**
+     * Returns all players from the dataset in undefined order as an unmodifiable List. If the dataset is empty, returns
+     * an empty List.
+     *
+     * @return the list of all players.
+     */
+    public List<Player> getAllPlayers() {
+        return players.collect(Collectors.toUnmodifiableList());
+    }
+
+    /**
+     * Returns an unmodifiable set of all nationalities in the dataset. If the dataset is empty, returns an empty Set.
+     *
+     * @return the set of all nationalities
+     */
+    public Set<String> getAllNationalities() {
+        return Set.copyOf(players.map((Player::nationality)).collect(Collectors.toSet()));
+    }
+
+    /**
+     * Returns the highest paid player from the provided nationality. If there are two or more players with equal
+     * maximum wage, returns any of them.
+     *
+     * @param nationality the nationality of the player to return
+     * @return the highest paid player
+     * @throws IllegalArgumentException in case the provided nationality is null
+     * @throws NoSuchElementException   in case there is no player with the provided nationality
+     */
+    public Player getHighestPaidPlayerByNationality(String nationality) {
+        if (nationality == null) {
+            throw new IllegalArgumentException("Nationality is null...");
+        }
+        Optional<Player> bestPlayer =
+            players.filter(player -> player.nationality().equals(nationality)).max((p1, p2) -> Math.toIntExact(
+                p1.wageEuro() - p2.wageEuro()));
+        if (!bestPlayer.isPresent()) {
+            throw new NoSuchElementException("No players from given nationality...");
+        }
+        return bestPlayer.get();
+    }
+
+    /**
+     * Returns a breakdown of players by position. Note that some players can play in more than one position so they
+     * should be present in more than one value Set. If no player plays in a given Position then that position should
+     * not be present as a key in the map.
+     *
+     * @return a Map with key: a Position and value: the set of players in the dataset that can play in that Position,
+     * in undefined order.
+     */
+    public Map<Position, Set<Player>> groupByPosition() {
+        Map<Position, Set<Player>> res = new EnumMap<>(Position.class);
+        for (Position position : Position.values()) {
+            res.put(position,
+                players.filter(player -> player.positions().contains(position)).collect(Collectors.toSet()));
+        }
+        return res;
+    }
+
+    /**
+     * Returns an Optional containing the top prospect player in the dataset that can play in the provided position and
+     * that can be bought with the provided budget considering the player's value_euro. If no player can be bought with
+     * the provided budget then return an empty Optional.
+     * <p>
+     * The player's prospect is calculated by the following formula: Prospect = (r + p) ÷ a where r is the player's
+     * overall rating, p is the player's potential and a is the player's age
+     *
+     * @param position the position in which the player should be able to play
+     * @param budget   the available budget for buying a player
+     * @return an Optional containing the top prospect player
+     * @throws IllegalArgumentException in case the provided position is null or the provided budget is negative
+     */
+    public Optional<Player> getTopProspectPlayerForPositionInBudget(Position position, long budget) {
+        if (position == null || budget < 0) {
+            throw new IllegalArgumentException("Bad arguments given...");
+        }
+        return players.filter(player -> player.positions().contains(position) && player.valueEuro() <= budget)
+            .max((p1, p2) -> p2.potential() - p1.potential());
+    }
+
+    /**
+     * Returns an unmodifiable set of players that are similar to the provided player. Two players are considered
+     * similar if: 1. there is at least one position in which both of them can play 2. both players prefer the same foot
+     * 3. their overall_rating measures differ by at most 3 (inclusive)
+     * If the dataset contains the provided player, the player will be present in the returned result.
+     *
+     * @param player the player for whom similar players are retrieved. It may or may not be part of the dataset.
+     * @return an unmodifiable set of similar players
+     * @throws IllegalArgumentException if the provided player is null
+     */
+    public Set<Player> getSimilarPlayers(Player player) {
+        return players.filter(p -> p.preferredFoot() == player.preferredFoot() &&
+                Collections.disjoint(player.positions(), p.positions()) &&
+                Math.abs(p.overallRating() - player.overallRating()) <= MAX_POTENTIAL_DIFFERENCE_FOR_SIMILARITY)
+            .collect(Collectors.toSet());
+    }
+
+    /**
+     * Returns an unmodifiable set of players whose full name contains the provided keyword (case-sensitive search)
+     *
+     * @param keyword the keyword that should be contained in player's full name
+     * @return an unmodifiable set of players
+     * @throws IllegalArgumentException if the provided keyword is null
+     */
+    public Set<Player> getPlayersByFullNameKeyword(String keyword) {
+        return players.filter(player -> player.fullName().contains(keyword)).collect(Collectors.toSet());
+    }
+
 }
--- Lab_08_2dec/test/bg/sofia/uni/fmi/mjt/football/FootballPlayerAnalyzerTest.java
+++ Lab_08_2dec/test/bg/sofia/uni/fmi/mjt/football/FootballPlayerAnalyzerTest.java
@@ -1,4 +1,13 @@
 package bg.sofia.uni.fmi.mjt.football;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class FootballPlayerAnalyzerTest {
+
+    @Test
+    void f() {
+        fail();
+    }
 }
