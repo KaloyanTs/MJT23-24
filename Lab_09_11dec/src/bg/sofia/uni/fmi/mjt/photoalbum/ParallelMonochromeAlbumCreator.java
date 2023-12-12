48e5f3f768b35e25db14743332998c116a3d3391
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2023-12-12 19:17:02 +0200

    Starting multiple threads

--- Lab_09_11dec/src/bg/sofia/uni/fmi/mjt/photoalbum/ParallelMonochromeAlbumCreator.java
+++ Lab_09_11dec/src/bg/sofia/uni/fmi/mjt/photoalbum/ParallelMonochromeAlbumCreator.java
@@ -1,29 +1,47 @@
 package bg.sofia.uni.fmi.mjt.photoalbum;
 
+import bg.sofia.uni.fmi.mjt.photoalbum.threads.ConsumerThread;
+
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ParallelMonochromeAlbumCreator implements MonochromeAlbumCreator {
 
     private final int imageProcessorsCount;
+    List<ConsumerThread> consumerThreads;
 
     public ParallelMonochromeAlbumCreator(int imageProcessorsCount) {
         this.imageProcessorsCount = imageProcessorsCount;
+        consumerThreads = new ArrayList<>(imageProcessorsCount);
+        for (int i = 0; i < imageProcessorsCount; ++i) {
+
+        }
     }
 
     @Override
     public void processImages(String sourceDirectory, String outputDirectory) {
         Path source = Path.of(sourceDirectory);
-        Path output = Path.of(outputDirectory);
         try (DirectoryStream<Path> stream = Files.newDirectoryStream(source, "*.jpg")) {
-            for (Path file : stream) {
-// todo implement producer consumer logic here
-                Image.saveImage(Image.loadImage(file), STR."\{outputDirectory}\\\{file.getFileName()}");
+            Iterator<Path> i = stream.iterator();
+            ConsumerThread t;
+            for (int j = 0; j < imageProcessorsCount; ++j) {
+                t = new ConsumerThread(i, outputDirectory);
+                consumerThreads.add(t);
+                t.start();
+            }
+            for (ConsumerThread thread : consumerThreads) {
+                thread.join();
             }
         } catch (IOException e) {
             throw new RuntimeException("Error while processing directory...", e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException("Thread interrupted...", e);
         }
     }
 }
