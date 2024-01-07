1ffbc713883c084ddd75e27f6e4317d9c50fa3e6
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2024-01-07 14:30:35 +0200

    Lab 11 and Homework 3 combined beginning

---
+++ Homework3/src/bg/sofia/uni/fmi/mjt/cookingcompass/recipe/Recipe.java
@@ -0,0 +1,14 @@
+package bg.sofia.uni.fmi.mjt.cookingcompass.recipe;
+
+public class Recipe {
+
+    private String label;
+    private final List<String> dietLabels;
+    private final List<String> healthLabels;
+    private final int totalWeight;
+    private final List<String> cuisineType;
+    private final String mealType;
+    private final String dishType;
+    private final List<String> ingredientLines;
+
+}
