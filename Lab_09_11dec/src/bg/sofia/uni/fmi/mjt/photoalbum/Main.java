deda5cd4e8b748d7b63283d6f478dac5a7853fef
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2024-01-07 14:30:57 +0200

    Lab 09 submitted, adding Main for testing

--- Lab_09_11dec/src/bg/sofia/uni/fmi/mjt/photoalbum/Main.java
+++ Lab_09_11dec/src/bg/sofia/uni/fmi/mjt/photoalbum/Main.java
@@ -2,10 +2,10 @@ package bg.sofia.uni.fmi.mjt.photoalbum;
 
 public class Main {
     public static void main(String[] args) {
-        ParallelMonochromeAlbumCreator p = new ParallelMonochromeAlbumCreator(5);
+        ParallelMonochromeAlbumCreator p = new ParallelMonochromeAlbumCreator(40);
 
         long d = System.nanoTime();
-        p.processImages("D:\\JavaProjects\\Lab_09_11dec\\picsFew", "D:\\JavaProjects\\Lab_09_11dec\\outPics");
+        p.processImages("D:\\JavaProjects\\Lab_09_11dec\\30small", "D:\\JavaProjects\\Lab_09_11dec\\outPics");
         System.out.println("Took me:\t" + (double) (System.nanoTime() - d) / 1_000_000_000 + "s");
     }
 }
