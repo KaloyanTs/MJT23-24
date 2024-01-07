25513ce3a2aaaff80c1570bf1b0f5e771f564079
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2024-01-07 18:40:36 +0200

    To be tested

--- Homework3/src/bg/sofia/uni/fmi/mjt/cookingcompass/Client.java
+++ Homework3/src/bg/sofia/uni/fmi/mjt/cookingcompass/Client.java
@@ -1,19 +1,15 @@
 package bg.sofia.uni.fmi.mjt.cookingcompass;
 
-import bg.sofia.uni.fmi.mjt.cookingcompass.recipe.Recipe;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Scanner;
 
 public class Client {
 
     public static void main(String[] args) {
 
-        System.out.println(RequestHandler.byKeywords(List.of("salad", "chicken", "alcohol-free")).size());
+        for (int i = 0; i < 1000; ++i) {
+            System.out.println(
+                RequestHandler.byKeywords(List.of("tomato", "biscuits%20and%20cookies", "dairy-free")).size());
+        }
 
     }
 }
