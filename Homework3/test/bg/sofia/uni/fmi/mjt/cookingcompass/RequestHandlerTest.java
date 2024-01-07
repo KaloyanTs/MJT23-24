ffc8e3cadaa1c2ccfd94632ac5c98b6b6e79a2ad
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2024-01-07 18:47:11 +0200

    Testing

---
+++ Homework3/test/bg/sofia/uni/fmi/mjt/cookingcompass/RequestHandlerTest.java
@@ -0,0 +1,15 @@
+package bg.sofia.uni.fmi.mjt.cookingcompass;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class RequestHandlerTest {
+
+    @Test
+    void testNothing() {
+        fail();
+    }
+
+
+}
