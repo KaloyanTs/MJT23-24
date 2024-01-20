aafe74f4566a1b444345dd82d0590d1b550c9ddc
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2024-01-20 13:02:34 +0200

    Trying to pass checkstyle

--- Homework3/src/bg/sofia/uni/fmi/mjt/cookingcompass/page/PageMover.java
+++ Homework3/src/bg/sofia/uni/fmi/mjt/cookingcompass/page/PageMover.java
@@ -3,6 +3,5 @@ package bg.sofia.uni.fmi.mjt.cookingcompass.page;
 import bg.sofia.uni.fmi.mjt.cookingcompass.response.RawResponse;
 
 public interface PageMover {
-
     String getNextPage(RawResponse element);
 }
